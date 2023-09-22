7d354a161fcf80bdb5ec2f7085e5ed0eabd4f2c2
Alain Gires Ebouel Nzom <aebouel@progi.com>
2023-09-22 19:22:16 -0400

    update

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-states-001"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    key    = "dev/terraform.tfstate"
+    region = "ca-central-1"
   }
 }
